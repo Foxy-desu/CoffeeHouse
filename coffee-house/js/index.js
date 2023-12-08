89adeb8b94366d59d810b70f08e3da4b9d8d0d2b
Aleksandra Orliuk <iammeduza@gmail.com>
2023-12-08 21:29:04 +0300

    feat: implement burger menu

--- coffee-house/js/index.js
+++ coffee-house/js/index.js
@@ -1,1 +1,23 @@
-console.log('hi!!');
+import burgerMenuHandler from "./handlers/burger-menu-handler";
+
+// //burger handler variables
+// const body = document.body;
+// const buttonOpen = document.querySelector('.burger');
+// const buttonClose = document.querySelector('.close-button');
+// const menu = document.querySelector('.header-block__nav');
+// const menuItems = menu.querySelectorAll('.navigation-item');
+
+// //event
+// buttonOpen.addEventListener('click', (event)=> {
+//     console.log(event.target);
+//     menu.classList.add('js-menu_active');
+//     body.classList.add('js-body_no-scroll');
+// });
+
+// buttonClose.addEventListener('click', (event)=> {
+//     console.log(event.target);
+//     menu.classList.remove('js-menu_active');
+//     body.classList.remove('js-body_no-scroll');
+// });
+
+burgerMenuHandler();
--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,21 @@ module.exports = {
     publicPath: '',
     filename: 'bundle.js'
   },
-  
+
+  module: {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /(node_modules)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env']
+              }
+            }
+          } 
+    ]
+  },
+
   mode: 'development'
 };
